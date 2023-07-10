3df25bf90a92abe6f89ea504352985d8d4ad059b
Martin Wimpress <martin@wimpress.org>
2023-07-10 08:43:10 +0100

    feat: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Dev tools for flexiondotorg.github.io";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          hugo                          # Terminal static site generator
+        ];
+      };
+    });
+}
